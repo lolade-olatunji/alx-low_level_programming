6aebdb32ba0da603a9e717cf38fc888a5c1f8048
lolade-olatunji <ololade0507@gmail.com>
2023-06-07 12:24:43 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	for (x = 0; temp && x < idx; x++)
+	for (x = 0; count && x < idx; x++)
 	{
 		if (x == idx - 1)
 		{
