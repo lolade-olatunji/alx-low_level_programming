efd29ed9369d58fd31d174aa61e1cd7bb5ae0fb6
lolade-olatunji <ololade0507@gmail.com>
2023-06-07 12:48:27 -0700

    task 103

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,32 +8,25 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	if (head == NULL || head->next == NULL)
-	{
-		return (NULL);
-	}
-
 	listint_t *slow = head;
 	listint_t *fast = head;
 
-	while (fast != NULL && fast->next != NULL)
+	if (!head)
+		return (NULL);
+
+	while (slow && fast && fast->next)
 	{
-		slow = slow->next;
 		fast = fast->next->next;
-
-
-		if (slow == fast)
+		slow = slow->next;
+		if (fast == slow)
 		{
 			slow = head;
-
-
 			while (slow != fast)
 			{
 				slow = slow->next;
 				fast = fast->next;
 			}
-
-			return (slow);
+			return (fast);
 		}
 	}
 	return (NULL);
